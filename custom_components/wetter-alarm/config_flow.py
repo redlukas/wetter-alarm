6155e9c43943f2a1a1da03d933005d1ce981e553
Matthias Studer <matthiaslukas.studer@students.bfh.ch>
2023-02-07 09:05:00 +0100

    add sensor and config flow

---
+++ custom_components/wetter-alarm/config_flow.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import logging
+
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.helpers.selector import TextSelector
+from homeassistant.helpers.selector import TextSelectorConfig
+from homeassistant.helpers.selector import TextSelectorType
+from homeassistant.util.network import is_ipv4_address
+from homeassistant.util.network import is_ipv6_address
+
+from . import WetterAlarmApiClient
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class WetterAlarmConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+    _pois = {}
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            poi_id = user_input.get("poi_id", "")
+            client = WetterAlarmApiClient(poi_id)
+            valid_poi = await client.validate_poi_id_async(
+                hass=self.hass
+            )
+            if valid_poi:
+                poi_name = user_input.get("poi_name", "")
+                return self.async_create_entry(
+                    title="Wetter-Alarm",
+                    data={
+                        "pois": {(poi_name, poi_id)}
+                    },
+                )
+            else:
+                _LOGGER.error("async step_user determined invalid POI")
+                errors["base"] = "invalid_connection"
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("poi_name"): str,
+                    vol.Required("poi_id"): int,
+                }
+            ),
+            errors=errors,
+        )
